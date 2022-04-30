996f4bb260e678ffbdf0a3de0d7ab82473162bb4
ImNyang <72956792+ImNyang@users.noreply.github.com>
2022-04-30 16:39:30 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,95 @@
 import os
+import discord
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix='!')
-
+bot = commands.Bot(command_prefix='냥 ')
+bot.remove_command('help')
 
 @bot.event
 async def on_ready():
-    print(f"Logged in as {bot.user}")
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+    user = await bot.fetch_user("909353223901569035")
+    await user.send("<:neko_party:943083727901302834>ㅣ봇이 준비되었습니다!")
 
-@bot.command()
+@bot.command(aliases=['핑', 'pong', '퐁'])
 async def ping(ctx):
-    await ctx.send('pong')
+    embed=discord.Embed(title="🏓ㅣ퐁!")
+    embed.add_field(name=f"{round(round(bot.latency, 4)*1000)}", value="ms", inline=True)
+    await ctx.reply(embed=embed)
 
-@bot.command()
-async def hello(ctx):
-    await ctx.send("Choo choo! 🚅")
+@bot.command(aliases=['도움', '도움말', '명령어'])
+async def help(ctx):
+    embed=discord.Embed(title="도움말", description="Prefix : `냥 `")
+    embed.add_field(name="`핑`, `퐁`, `ping`, `pong`", value="퐁!", inline=True)
+    embed.add_field(name="`청소`, `지워`, `삭제`, `clean`, `clear`", value="챗을 정리합니다. (최대 갯수 없음 하지만 렉으로 인한 봇이 죽을 가능성 있음)", inline=True)
+    embed.add_field(name="`밴`, `죽어라`, `ven`, 'ban'", value="유저를 ven합니다!", inline=True)
+    embed.add_field(name="`언밴`, `살어라`, `unven`, `unban`", value="유저를 unven합니다!", inline=True)
+    embed.add_field(name="`킥`, 'kick'", value="유저를 킥합니다!", inline=True)
+    await ctx.reply(embed=embed)
 
+@bot.command(aliases=['Hi','hi','Hello', 'hello', '안녕하세요'])
+async def 안녕(ctx):
+    await ctx.reply("반갑습니다 {}님! 저는 ImBot입니다! 궁금한점이 있다면 `냥 help`를 입력해주세요!".format(
+        ctx.author.name))
+
+@bot.command(aliases=['테스트'])
+async def test(ctx):
+   msg = await ctx.reply(f"{round(round(bot.latency, 4)*1000)}ms 테스트 완료! 이 메시지는 3초 뒤에 삭제됩니다.")
+   await msg.delete(delay=3)
+
+import datetime
+
+now = datetime.datetime.now()
+time = f"{str(now.year)}년 {str(now.month)}월 {str(now.day)}일 {str(now.hour)}시 {str(now.minute)}분 {str(now.second)}초"
+
+@bot.event
+async def on_message_delete(message):
+    channel = bot.get_channel(969520638513520690)
+    embed = discord.Embed(title=f"삭제됨", description=f"유저 : {message.author.mention} 채널 : {message.channel.mention}", color=0xFF0000)
+    embed.add_field(name="삭제된 내용", value=f"내용 : {message.content}", inline=False)
+    embed.set_footer(text=f"{message.guild.name} | {time}")
+    await channel.send(embed=embed)
+
+#969520638513520690
+
+@bot.event
+async def on_message_edit(before, after):
+    channel = bot.get_channel(969520638513520690)
+    embed = discord.Embed(title=f"수정됨", description=f"유저 : {before.author.mention} 채널 : {before.channel.mention}", color=0xFF9900)
+    embed.add_field(name="수정 전 내용", value=before.content, inline=True)
+    embed.add_field(name="수정 후 내용", value=after.content, inline=True)
+    embed.set_footer(text=f"{before.guild.name} | {time}")
+    await channel.send(embed=embed)
+
+@commands.has_permissions(kick_members=True)
+@bot.command(aliases=['청소','clean','지워','삭제'])
+async def clear(ctx, amount : int):
+    await ctx.channel.purge(limit=amount)
+    msg = await ctx.send("<:neko_candy:943083727985180722>ㅣ완료되었습니다! 이 메시지도 곧 삭제됩니다.")
+    await msg.delete(delay=3)
+
+@bot.command(aliases=['밴','ven','죽어라'])
+@commands.has_permissions(ban_members = True)
+async def ban(ctx, user: discord.Member, *, reason="No reason provided"):
+    await user.ban(reason=reason)
+    await ctx.channel.send(f"`sudo ven {user.name} && reason {reason}`")
+    await user.send(f"`sudo ven {user.name} && reason {reason}`")
+
+@bot.command(aliases=['언밴','unven','살아라'])
+@commands.has_permissions(administrator=True)
+async def unban(ctx, *, member_id: int):
+    """ command to unban user. check !help unban """
+    await ctx.guild.unban(discord.Object(id=member_id))
+    await ctx.reply(f"`sudo unven {member_id}`")
+
+@commands.has_permissions(kick_members=True)
+@bot.command(aliases=['킥'])
+async def kick(ctx, user: discord.Member, *, reason="No reason provided"):
+    await user.kick(reason=reason)
+    await ctx.channel.send(f"`sudo kick {user.name} && reason {reason}`")
+    await user.send(f"`sudo kick {user.name} && reason {reason}`")
 
 bot.run(os.environ["DISCORD_TOKEN"])
