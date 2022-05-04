155fc071a8e5cfb4c01acdc9526cfcd2bf22dfd7
ImNyang <72956792+ImNyang@users.noreply.github.com>
2022-05-04 17:46:13 +0900

    Update 2 Files

--- main.py
+++ main.py
@@ -4,9 +4,36 @@ from discord.ext import commands
 import random
 import platform
 import psutil
+import asyncio
+import requests
+from discord.ext.commands import Bot
+from discord import Game
+from bs4 import BeautifulSoup
+from urllib.parse import unquote
 
 bot = commands.Bot(command_prefix='냥 ')
 bot.remove_command('help')
+
+async def Google(q):
+	searchpage = requests.get("https://www.google.co.kr/search?q={}".format(q))
+	bsoup = BeautifulSoup(searchpage.content, 'html.parser')
+	message = ""
+	results = bsoup.findAll("div", attrs={"class":"g"})
+	for result in results:
+		h3 = result.find("h3", attrs={"class":"r"}, recursive=True)
+		if h3 != None:
+			a = h3.find("a")
+			txt = h3.text
+			link = a.get("href")
+			link = link.replace("/url?q=", "")
+			link = link.replace("/search?", "https://www.google.co.kr/search?")
+			link = link[:link.rfind("&sa=U")]
+			link = unquote(link)
+			message += "{}\n{}\n\n".format(txt,link)
+			if len(message) > 1600:
+				break
+	return message
+
 
 @bot.event
 async def on_ready():
@@ -142,5 +169,17 @@ async def info(ctx):
     embed.add_field(name="Ram", value=str(round(psutil.virtual_memory().total / (1024.0 **3)))+"(GB)", inline=False)
     embed.set_footer(text="Railway.app")
     await ctx.send(embed=embed)
+
+@bot.command(pass_context=True)
+async def google(context, args="구글"):
+    if len(args) > 1:
+        try:
+            searchQuery = "".join(args[0:])
+            messageString = await Google(searchQuery)
+            em = discord.embed(title="\"{}\"에 대한 구글 검색결과".format(searchQuery), description=messageString, colour=0x8FACEf)
+            await bot.send_message(context.message.channel, embed=em)
+        except:
+            embed=discord.Embed(title="Error", description="결과를 찾을 수 없어요...")
+            await bot.send_message(context.message.channel, embed=embed)
 
 bot.run(os.environ["DISCORD_TOKEN"])
