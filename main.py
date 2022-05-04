7997d6513621657b1ae9a8bd8d3b111047a6159a
ImNyang <72956792+ImNyang@users.noreply.github.com>
2022-05-04 16:29:30 +0900

    Update main.py

--- main.py
+++ main.py
@@ -137,6 +137,11 @@ async def dice(ctx):
 
 @bot.command(aliases=['정보'])
 async def info(ctx):
-    await ctx.reply("OS : "+platform.system())
+    embed=discord.Embed(title="정보", description="railway.app으로 호스팅 중")
+    embed.add_field(name="운영체제", value="OS" + platform.system() + " : " + platform.version(), inline=False)
+    embed.add_field(name="CPU", value=platform.processor(), inline=False)
+    embed.add_field(name="Ram", value=str(round(psutil.virtual_memory().total / (1024.0 **3)))+"(GB)", inline=False)
+    embed.set_footer(text="Railway.app")
+    await ctx.send(embed=embed)
 
 bot.run(os.environ["DISCORD_TOKEN"])
