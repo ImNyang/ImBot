5a3b90414981da19988f1dfc6e77ac4372e034d3
ImNyang <72956792+ImNyang@users.noreply.github.com>
2022-05-04 18:01:09 +0900

    Update main.py
    - 구글 검색 (작동 X)
    + jishaku 툴 추가

--- main.py
+++ main.py
@@ -5,35 +5,11 @@ import random
 import platform
 import psutil
 import asyncio
-import requests
-from discord.ext.commands import Bot
-from discord import Game
-from bs4 import BeautifulSoup
-from urllib.parse import unquote
+import jishaku
 
 bot = commands.Bot(command_prefix='냥 ')
 bot.remove_command('help')
-
-async def Google(q):
-	searchpage = requests.get("https://www.google.co.kr/search?q={}".format(q))
-	bsoup = BeautifulSoup(searchpage.content, 'html.parser')
-	message = ""
-	results = bsoup.findAll("div", attrs={"class":"g"})
-	for result in results:
-		h3 = result.find("h3", attrs={"class":"r"}, recursive=True)
-		if h3 != None:
-			a = h3.find("a")
-			txt = h3.text
-			link = a.get("href")
-			link = link.replace("/url?q=", "")
-			link = link.replace("/search?", "https://www.google.co.kr/search?")
-			link = link[:link.rfind("&sa=U")]
-			link = unquote(link)
-			message += "{}\n{}\n\n".format(txt,link)
-			if len(message) > 1600:
-				break
-	return message
-
+bot.load_extension('jishaku')
 
 @bot.event
 async def on_ready():
@@ -170,16 +146,6 @@ async def info(ctx):
     embed.set_footer(text="Railway.app")
     await ctx.send(embed=embed)
 
-@bot.command(pass_context=True)
-async def google(context, args="구글"):
-    if len(args) > 1:
-        try:
-            searchQuery = "".join(args[0:])
-            messageString = await Google(searchQuery)
-            em = discord.embed(title="\"{}\"에 대한 구글 검색결과".format(searchQuery), description=messageString, colour=0x8FACEf)
-            await bot.send_message(context.message.channel, embed=em)
-        except:
-            embed=discord.Embed(title="Error", description="결과를 찾을 수 없어요...")
-            await bot.send_message(context.message.channel, embed=embed)
+
 
 bot.run(os.environ["DISCORD_TOKEN"])
