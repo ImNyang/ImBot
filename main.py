a294a3737daa3e8834b051499522a587f82706c5
ImNyang <72956792+ImNyang@users.noreply.github.com>
2022-05-10 12:55:57 +0900

    Update
    겁나 대규모 업데이트
    
    Profile 만듬

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 #라이브러리 안쓰는거 같지만 다 씀
 
+from calendar import c
+from sys import builtin_module_names
+from turtle import circle
 import discord
 from discord.ext import commands
 import random, platform, psutil, asyncio, jishaku, os
 from discord_buttons_plugin import *
+from PIL import Image, ImageChops, ImageDraw, ImageFont
+from io import BytesIO
 
 #함수 설정
 
@@ -11,6 +16,19 @@ bot = commands.Bot(command_prefix='냥 ',)
 buttons = ButtonsClient(bot)
 bot.remove_command('help')
 bot.load_extension('jishaku')
+
+def circle(pfp, size=(215,215)):
+
+    pfp = pfp.resize(size, Image.ANTIALIAS).convert("RGBA")
+
+    bigsize = (pfp.size[0] * 3, pfp.size[1] * 3)
+    mask = Image.new('L', bigsize, 0)
+    draw = ImageDraw.Draw(mask)
+    draw.ellipse((0, 0) + bigsize, fill=255)
+    mask = mask.resize(pfp.size, Image.ANTIALIAS)
+    mask = ImageChops.darker(mask, pfp.split()[-1])
+    pfp.putalpha(mask)
+    return pfp
 
 #event 처리
 
@@ -208,4 +226,69 @@ async def GitHub(ctx):
 	    ]
     )
 
+@bot.command() 
+async def add(ctx, *nums):
+    operation = " + ".join(nums)
+    await ctx.send(f'{operation}')
+
+@bot.command() 
+async def sub(ctx, *nums): 
+    operation = " - ".join(nums)
+    await ctx.send(f'{operation}')
+
+@bot.command() 
+async def multiply(ctx, *nums): 
+    operation = " * ".join(nums)
+    await ctx.send(f'{operation}')
+
+@bot.command() 
+async def divide(ctx, *nums): 
+    operation = " / ".join(nums)
+    await ctx.send(f'{operation}')
+
+@bot.command()
+async def profile(self, ctx, member:discord.Member=None):
+    if not member:
+        member = ctx.author
+
+    name, nick, Id, status = str(member), member.display_name,str(member.id), str(member.status).upper()
+    
+    created_at = member.created_at.strftime("%a %b\n%B %Y")
+    joined_at = member.joined_at.strftime("%a %b\n%B %Y")
+
+    money, level = "IDK","IDK"
+
+    base = Image.open("base.png").convert("RGBA")
+    background = Image.open("base.png").convert
+
+    pfp = member.avatar_url_as(size=256)
+    data=BytesIO(await pfp.read())
+    pfp = Image.open(data).convert("RGBA")
+
+    name = f"{name[:16]}.." if len(name)>16 else name
+    nick = f"AKA - {name[:17]}.." if len(name)>17 else f"AKA - {nick}"
+    
+    draw = ImageDraw.Draw(base)
+    pfp = circle(pfp, (215,215))
+    font = ImageFont.truetype("font.ttf", 38)
+    akafont = ImageFont.truetype("font.ttf", 30)
+    akafont = ImageFont.truetype("font.ttf", 30)
+    subfont = ImageFont.truetype("font.ttf", 25)
+
+    draw.text((280, 240),name,font=font)
+    draw.text((270, 315),nick,font=akafont)
+    draw.text((65, 490),Id,font=subfont)
+    draw.text((405, 490),status,font=subfont)
+    draw.text((65, 635),money,font=subfont)
+    draw.text((405, 635),level,font=subfont)
+    draw.text((65, 770),created_at,font=subfont)
+    draw.text((405, 770),joined_at,font=subfont)
+    base.paste(pfp,(56,158), pfp)
+
+    with BytesIO() as a:
+        background.save(a, "PNG")
+        a.seek(0)
+        await ctx.reply(file = discord.File(a, "profile.png"))
+    
+    
 bot.run(os.environ["DISCORD_TOKEN"])
