5a7fadaaf6e526cf8b07c007d2e024e18ae5f3e0
ImNyang <72956792+ImNyang@users.noreply.github.com>
2022-05-19 17:26:57 +0900

    Update event.py
    event 오류 해결

--- cogs/event.py
+++ cogs/event.py
@@ -6,7 +6,7 @@ class Event(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.event
+    @commands.Cog.listener()
     async def on_ready(self):
         print('Logged in as')
         print(self.bot.user.name)
@@ -16,8 +16,8 @@ class Event(commands.Cog):
         await user.send("<:neko_party:943083727901302834>ㅣ봇이 준비되었습니다!")
         await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="냥 도움말"))
 
-    @commands.event
-    async def on_command_error(ctx, error):
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx, error):
         embed=discord.Embed(title="Error!", description="어... 이게 무슨 상황인지 개발자에게 알려주세요!")
         embed.add_field(name="오류 내용", value=f"`{str(error)}`", inline=True)
         embed.set_footer(text="Dm : ImNyang#9009")
